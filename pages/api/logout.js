b1742888af59080cfa19c2da1ac830bf656952c7
CarsonLenze <carsonlenze@gmail.com>
2022-05-23 14:40:54 -0400

    Update logout.js

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,7 +3,7 @@ const { verify } = require("jsonwebtoken");
 
 export default async function handler(req, res) {
     verify(req.cookies.session, process.env.COOKIE_SECRET, async function (err, user) {
-        if (err) return  res.send({ status: 'error' });
+        if (err) return res.send({ status: 'error' });
 
         const params = new URLSearchParams({
             client_id: process.env.CLIENT_ID,
@@ -12,7 +12,12 @@ export default async function handler(req, res) {
         });
 
         await axios.post("https://discord.com/api/oauth2/token/revoke", params).catch(() => { /* here to prevent crashes */ });
-        res.setHeader("Set-Cookie", "session=; Max-Age=-1; Path=/; SameSite=lax;");
+        // res.cookie('session', '', {
+        //     httpOnly: true,
+        //     maxAge: -1,
+        //     Path: '/'
+        //   });
+        res.setHeader("Set-Cookie", "test=hey; Path=/;");
 
         return res.send({ status: 'good' });
     });
