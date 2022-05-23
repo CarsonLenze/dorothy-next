428f171891dec83157e3d8066c265c8c9e9a2852
CarsonLenze <carsonlenze@gmail.com>
2022-05-23 16:16:24 -0400

    Delete _middleware.js

--- pages/api/_middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function middleware() {
-    const resp = NextResponse.next();
-    resp.headers.append("Set-Cookie", "cookie1=val1");
-    resp.headers.append("Set-Cookie", "cookie2=val2");
-    return resp;
-}
